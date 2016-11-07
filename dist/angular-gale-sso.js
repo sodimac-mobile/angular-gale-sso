f6a10833843bbb49b92126e57687ab4106575965
David Munoz Gaete <dmunozgaete@gmail.com>
2016-11-07 15:45:57 -0300

    login en componente

--- dist/angular-gale-sso.js
+++ dist/angular-gale-sso.js
@@ -6,13 +6,13 @@
  Github:            https://github.com/dmunozgaete/angular-gale-sso
 
  Versión:           1.0.0-rc.1
- Build Date:        2016-10-20 11:53:59
+ Build Date:        2016-11-07 15:42:09
 ------------------------------------------------------*/
 
 angular.module('gale-sso.templates', []).run(['$templateCache', function($templateCache) {
   "use strict";
   $templateCache.put("gale-sso/gale-sso.tpl.html",
-    "<flex-loading ng-if=model.isLoading></flex-loading><iframe frameborder=0 border=0 cellspacing=0 ng-show=!model.isLoading></iframe>");
+    "<sso-loading ng-if=model.isLoading><sso-image><img src=\"data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHdpZHRoPSc3MnB4JyBoZWlnaHQ9JzcycHgnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDEwMCAxMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaWRZTWlkIiBjbGFzcz0idWlsLXJpbmctYWx0Ij48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgZmlsbD0ibm9uZSIgY2xhc3M9ImJrIj48L3JlY3Q+PGNpcmNsZSBjeD0iNTAiIGN5PSI1MCIgcj0iNDAiIHN0cm9rZT0ibm9uZSIgZmlsbD0ibm9uZSIgc3Ryb2tlLXdpZHRoPSIxMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIj48L2NpcmNsZT48Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiByPSI0MCIgc3Ryb2tlPSIjMDA3MGQxIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjYiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCI+PGFuaW1hdGUgYXR0cmlidXRlTmFtZT0ic3Ryb2tlLWRhc2hvZmZzZXQiIGR1cj0iM3MiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiBmcm9tPSIwIiB0bz0iNTAyIj48L2FuaW1hdGU+PGFuaW1hdGUgYXR0cmlidXRlTmFtZT0ic3Ryb2tlLWRhc2hhcnJheSIgZHVyPSIzcyIgcmVwZWF0Q291bnQ9ImluZGVmaW5pdGUiIHZhbHVlcz0iMTUwLjYgMTAwLjQ7MSAyNTA7MTUwLjYgMTAwLjQiPjwvYW5pbWF0ZT48L2NpcmNsZT48L3N2Zz4=\"></sso-image><sso-title>Iniciando Plataforma</sso-title><sso-legend>Espere por favor...</sso-legend></sso-loading><iframe frameborder=0 border=0 cellspacing=0 ng-show=!model.isLoading></iframe>");
 }]);
 ;angular.manifiest('gale-sso', [
     'gale-sso.templates',
@@ -85,9 +85,6 @@ angular.module('gale-sso.templates', []).run(['$templateCache', function($templa
                 }, 10);
 
             });
-
-
-
 
             //Garbage Collector Destroy
             $scope.$on('$destroy', function() {
